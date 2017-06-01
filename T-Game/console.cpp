04c9d0553f2fc013d475d3b5ac99f58f461043c3
w-chan <wchan@enseirb-matmeca.fr>
2017-06-01 12:18:32 +0200

    First console
    Visual Studio
    Open a console

---
+++ T-Game/console.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Open a console, print Hello World and Bye, wait for a key pressed
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Bye" << std::endl;
+	std::cin.get();
+	return 0;
+}
